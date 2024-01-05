f70029ac0b306e6e21a6ac842b83e6c6c3aa2d20
CaneToads <adhirajasa.hafiz@gmail.com>
2024-01-05 16:30:25 +0700

    Submission5

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    public function store(Request $request)
+    {
+        $task = new Task();
+        $task->title = $request->input('title');
+        $task->description = $request->input('description');
+        $task->save();
+
+        return redirect('/tasks');
+    }
+
+    public function edit($id)
+    {
+        $task = Task::find($id);
+        return view('tasks.edit', compact('task'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::find($id);
+        $task->title = $request->input('title');
+        $task->description = $request->input('description');
+        $task->save();
+
+        return redirect('/tasks');
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+
+        return redirect('/tasks');
+    }
+}
